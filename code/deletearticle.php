53f931e1c9af887afe17814a6532e73502dc3758
kevjbaumgartner <kevin.joseph.baumgartner@gmail.com>
2020-02-23 00:00:33 -0500

    Attempting to resolve CSRF on deletearticle.php WIP

--- code/deletearticle.php
+++ code/deletearticle.php
@@ -3,11 +3,19 @@ session_start();
 include("config.php");
 include("lib/db.php");
 
+$tempn = 16;
+$tempc = TRUE;
+$tempkey = bin2hex(openssl_random_pseudo_bytes($tempn, $tempc));
+$_SESSION['pseudo'] = $tempkey;
+
+
+
+if($_GET['token'] == $_SESSION['pseudo']){
 $aid = $_GET['aid'];
 #echo "aid=".$aid."<br>";
 $result = delete_article($dbconn, $aid);
 #echo "result=".$result."<br>";
 # Check result
 header("Location: /admin.php");
-
+}
 ?>
