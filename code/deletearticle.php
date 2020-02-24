b2804738643f23fd7e469d784ac80a3e732b9397
mharquail <mharquail95@gmail.com>
2020-02-24 10:47:43 -0500

    Fixed issue where non logged in user could still perform CSRF

--- code/deletearticle.php
+++ code/deletearticle.php
@@ -3,10 +3,12 @@ session_start();
 include("config.php");
 include("lib/db.php");
 
-if($_GET['token'] == $_SESSION['token']){
-	$aid = $_GET['aid'];
-	$result = delete_article($dbconn, $aid);
-	header("Location: /admin.php");
+if(!empty($_SESSION['token'])){
+	if($_GET['token'] == $_SESSION['token']){
+		$aid = $_GET['aid'];
+		$result = delete_article($dbconn, $aid);
+		header("Location: /admin.php");
+	}
 }
 else{
 	header("Location: /admin.php");
